d1d72ccf129fe3875c531a44108ad7408f9aef90
Danny <dgimeno777@gmail.com>
2020-02-12 22:41:20 -0500

    add custom message reply

--- python-bot/gimeno_bot1/gimeno_bot1.py
+++ python-bot/gimeno_bot1/gimeno_bot1.py
@@ -50,7 +50,7 @@ def handle_message(event):
     if msg_from_user == 'hello':
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage
+            TextSendMessage(text='hi')
         )
     else:
         line_bot_api.reply_message(
